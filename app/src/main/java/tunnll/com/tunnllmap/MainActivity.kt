750954c0652445b0e1df602fca85a59c0257f0af
Nemanja Simic <nemanjaasimic@gmail.com>
2017-08-31 22:47:22 +0200

    Initial commit

---
+++ app/src/main/java/tunnll/com/tunnllmap/MainActivity.kt
@@ -0,0 +1,13 @@
+package tunnll.com.tunnllmap
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import tunnll.com.tunnllmap.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
