6d42906245718d4748031b9d2cadbab7c206ce8e
Nemanja Simic <nemanjaasimic@gmail.com>
2017-09-05 23:08:50 +0200

    Shows a map

--- app/src/main/java/tunnll/com/tunnllmap/activity/MainActivity.kt
+++ app/src/main/java/tunnll/com/tunnllmap/activity/MainActivity.kt
@@ -2,12 +2,36 @@ package tunnll.com.tunnllmap.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import org.osmdroid.api.IMapController
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 import tunnll.com.tunnllmap.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ctx = applicationContext
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
         setContentView(R.layout.activity_main)
+
+        val map: MapView = findViewById(R.id.map) as MapView
+        map.setTileSource(TileSourceFactory.MAPNIK)
+
+        map.setBuiltInZoomControls(true)
+        map.setMultiTouchControls(true)
+
+        val mapController: IMapController = map.controller
+        mapController.setZoom(9)
+        val startPoint = GeoPoint(48.2, 2.92)
+        mapController.setCenter(startPoint)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
     }
 }
--- app/src/main/java/tunnll/com/tunnllmap/view/MainView.kt
+++
@@ -1,6 +0,0 @@
-package tunnll.com.tunnllmap.view
-
-import android.support.v7.app.AppCompatActivity
-
-class MainView : AppCompatActivity() {
-}
